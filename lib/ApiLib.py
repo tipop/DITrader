a0c46b4ebc9a1b632a57cb19d42d9bc49c0b55ef
tipop7 <tipop7@gmail.com>
2021-09-02 14:46:11 +0900

    Lib에 getLowestDIPastDay 함수 추가 (사용하지 않음)

--- lib/ApiLib.py
+++ lib/ApiLib.py
@@ -3,6 +3,7 @@ import datetime as dt
 import time
 import pandas as pd
 import pprint
+import datetime
 
 class Lib:
     def init(apiKeyFilePath):
@@ -54,12 +55,12 @@ class Lib:
             print(now, "\t", btc['last']) # 현재가 ex) 48800.00
             time.sleep(1)   # 1초
 
-    def get_1min_close_20(symbol):
+    def get_1min_close(symbol, count):
         ohlc = Lib.api.fetch_ohlcv(
                 symbol = symbol,
                 timeframe = '1m',
-                since = None,
-                limit = 20)
+                since = None,   # 1분봉은 최대 24시간 전만 가능하므로 since는 무의미
+                limit = count)  # 1500개가 한계다
 
         # pprint.pprint(ohlc)
             
@@ -80,11 +81,34 @@ class Lib:
         return sum(close_prices) / len(close_prices)
 
     def get20Ma(symbol, curPrice):
-        df = Lib.get_1min_close_20(symbol)
+        df = Lib.get_1min_close(symbol, 20)
         close_1min_20_list = df['close'].to_list()
         close_1min_20_list[-1] = curPrice # 현재가로 교체하여 보정
         return Lib.get_moving_average(close_1min_20_list)
 
+    def getLowestDIPastDay(symbol):  # 지난 몇 시간동안 최대 이격도를 구한다. (1분봉 20이평에서 하락 이격도)
+        lowestDI = 0
+        from20m = 0
+        to20m = 20
+
+        df = Lib.get_1min_close(symbol, 60 * 24)    # 1분봉은 최대 24시간만 가능함.
+
+        while to20m < len(df.index):
+            df20 = df.iloc[from20m:to20m]
+            ma20 = sum(df20['close']) / len(df20['close'])
+            low = df20.iloc[-1,2]   # 20번째 봉의 low 값
+            di = (low - ma20) / ma20
+            
+            if lowestDI > di:
+                lowestDI = di
+            
+            from20m += 1
+            to20m += 1
+        
+        # print("lowest DI:", lowestDI * 100, "%")
+        return lowestDI
+
+        
 
     ####### 트레이링 스탑 동작 함###############
         #rate = '0.5'
