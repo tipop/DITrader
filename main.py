1250535dca461a5056a7cd922a45cfe17be21e83
tipop7 <tipop7@gmail.com>
2021-09-09 21:49:23 +0900

    CatchBot만 사용. 1스레드로 변경.

--- main.py
+++ main.py
@@ -23,26 +23,20 @@ def parseJsOption(jsOption):
         stoplossTriggerPercent = jsOption['stoplossTriggerPercent'],
         stoplossPercent = 0.001)
 
-def startBucketBot(symbol, option):
-    BucketBot(symbol, option).start()
-
-def startCatchBot(symbol, option):
-    CatchBot(symbol, option).start()
+def startCatchBot(symbols, option):
+    CatchBot(symbols, option).start()
 
 def getStartMsg():
-    return "Start!!\nCatch: {} | Bucket: {} | Symbols: {}\n\
-    BucketBot\n\
-        - TargetDI: {:.1f}%\n\
-        - profit: {:.1f}%\n\
-        - stoplossTrigger: {:.1f}%\n\
-        - marginRatio: {:.1f}%\n\
-    CatchBot\n\
-        - TargetDI: {:.1f}%\n\
-        - profit: {:.1f}%\n\
-        - stoplossTrigger: {:.1f}%\n\
-        - marginRatio: {:.1f}%".format(js['useCatchBot'], js['useBucketBot'], len(js['symbols']), 
-            bucketOption.targetDI*100, bucketOption.profitPercent*100, bucketOption.stoplossTriggerPercent*100, bucketOption.marginRatio*100,
-            catchOption.targetDI*100, catchOption.profitPercent*100, catchOption.stoplossTriggerPercent*100, catchOption.marginRatio*100)
+    return "CatchBot Start!!\nSymbols: {}\n\
+    - TargetDI: {:.1f}%\n\
+    - profit: {:.1f}%\n\
+    - stoplossTrigger: {:.1f}%\n\
+    - marginRatio: {:.1f}%".format(
+        len(js['symbols']),
+        catchOption.targetDI*100,
+        catchOption.profitPercent*100, 
+        catchOption.stoplossTriggerPercent*100, 
+        catchOption.marginRatio*100)
 
 def panicSellAlarm(symbols, deltaForAlarm):
     prevPriceList = {}
@@ -80,26 +74,20 @@ def panicSellAlarm(symbols, deltaForAlarm):
 js = readJsonSetting("trade_setting.json")
 Lib.init(js['binanceApi']['key'], js['binanceApi']['secret'])
 catchOption = parseJsOption(js['catch'][0])   # 1번 DI 타겟
-bucketOption = parseJsOption(js['bucket'])
 
 logger.remove()
 logger.add('logs/log.log', level = js['log']['fileLevel'])
 logger.add(sys.stderr, level = js['log']['consoleLevel'])
-logger.info("CatchBot: {} | BucketBot: {} | Symbols: {}", js['useCatchBot'], js['useBucketBot'], len(js['symbols']))
+logger.info("Symbols: {}", len(js['symbols']))
 TelegramBot.init(js['telegramToken'])
 
 if js['useCatchBot']:
-    for coin in js["symbols"]:
-        threading.Thread(target = startCatchBot, args = (coin, catchOption)).start()
+    threading.Thread(target = startCatchBot, args = (js["symbols"], catchOption)).start()
 
-if js['useBucketBot']:    
-    for coin in js["symbols"]:
-        threading.Thread(target = startBucketBot, args=(coin, bucketOption)).start()
+if js['panicSellAlarm']['use']:
+    threading.Thread(target = panicSellAlarm, args=(js["symbols"], js['panicSellAlarm']['deltaPercent'])).start()
 
 msg = getStartMsg()
 logger.info("TelegramOn: {}", TelegramBot.isChatOn)
 logger.info(msg)
 TelegramBot.sendMsg(msg)
-
-if js['panicSellAlarm']['use']:
-    threading.Thread(target = panicSellAlarm, args=(js["symbols"], js['panicSellAlarm']['deltaPercent'])).start()
